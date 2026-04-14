a5fa485016e84e910e995aec3debe9cea20132d4
Karyl Chesman <karylbps@gmail.com>
2026-04-13 21:45:16 -0300

    feat: added a new ingestion pipeline for gathering financial news from yahoo finance

---
+++ ingestion/news_ingestion.py
@@ -0,0 +1,62 @@
+import os
+import uuid
+
+from dotenv import load_dotenv
+from fastembed import LateInteractionTextEmbedding, SparseTextEmbedding, TextEmbedding
+from qdrant_client import QdrantClient, models
+from utils.news_client import NewsClient
+from utils.simple_chunker import SimpleChunker
+
+load_dotenv()
+
+DENSE_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+SPARSE_MODEL = "Qdrant/bm25"
+COLBERT_MODEL = "colbert-ir/colbertv2.0"
+COLLECTION_NAME = "financial"
+MAX_TOKENS = 300
+
+qdrant_client = QdrantClient(
+    url=os.getenv("QDRANT_HOST"),
+    api_key=os.getenv("QDRANT_API_KEY"),
+)
+
+news_client = NewsClient()
+news_data = news_client.fetch_news(ticker="AAPL", max_stories=10)
+
+chunker = SimpleChunker(max_tokens=MAX_TOKENS)
+
+all_chunks = []
+for article in news_data:
+    chunks = chunker.create_chunks(article["text"])
+    for chunk in chunks:
+        all_chunks.append({"text": chunk, "metadata": article["metadata"]})
+
+dense_model = TextEmbedding(model_name=DENSE_MODEL)
+sparse_model = SparseTextEmbedding(model_name=SPARSE_MODEL)
+colbert_model = LateInteractionTextEmbedding(model_name=COLBERT_MODEL)
+
+points = []
+for chunk_data in all_chunks:
+    chunk = chunk_data["text"]
+    metadata = chunk_data["metadata"]
+
+    dense_embedding = list(dense_model.passage_embed([chunk]))[0].tolist()
+    sparse_embedding = list(sparse_model.passage_embed([chunk]))[0].as_object()
+    colbert_embedding = list(colbert_model.passage_embed([chunk]))[0].tolist()
+    point = models.PointStruct(
+        id=str(uuid.uuid4()),
+        vector={
+            # type: ignore Pylance isn't recognizing the dict structure type,
+            # expects Dict[str, float] but numpy is returning Dict[str, NumpyArray]
+            "dense": dense_embedding,
+            "sparse": sparse_embedding,
+            "colbert": colbert_embedding,
+        },
+        payload={"text": chunk, "metadata": metadata},
+    )
+    points.append(point)
+
+
+qdrant_client.upload_points(
+    collection_name=COLLECTION_NAME, points=points, batch_size=5
+)
